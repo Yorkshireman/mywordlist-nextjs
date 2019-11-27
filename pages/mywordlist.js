4609b5eb51b68358fb63143a676cdac309325468
Andrew Stelmach <Andrew.Stelmach@sky.uk>
2019-11-27 23:55:06 +0000

    Implement basic layout and page flow.
    * keep root path page but move wordlist onto a /wordlist route and page

---
+++ pages/mywordlist.js
@@ -0,0 +1,6 @@
+import Layout from '../components/Layout';
+import MyWordlist from '../components/MyWordlist';
+
+const MyWordlistView = () => <Layout><MyWordlist /></Layout>;
+
+export default MyWordlistView;
