8d4cd4fc77c339ca4b369b9510ac5a73883b0aff
Andrew Stelmach <Andrew.Stelmach@sky.uk>
2019-11-30 13:25:59 +0000

    use style jsx

--- components/Footer.js
+++ components/Footer.js
@@ -1,13 +1,15 @@
 const navBarStyle = {
-  backgroundColor: 'red',
-  color: 'white',
-  width: '100%',
-  height: '60px'
 };
 
 const NavBar = () => (
   <div className='NavBar' style={navBarStyle}>
-    NAVBAR
+    FOOTER
+    <style jsx>{`
+      background-color: red;
+      color: white;
+      width: 100%;
+      height: 60px;
+    `}</style>
   </div>
 );
 
