0df2eb3e3a33ae07138baeb65035264dfb3926f1
Andrew Stelmach <Andrew.Stelmach@sky.uk>
2019-12-03 22:36:05 +0000

    remove modal

--- components/MyWordlist.js
+++ components/MyWordlist.js
@@ -1,4 +1,3 @@
-import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
 import Error from 'next/error';
 import React, { Component } from 'react';
 
@@ -7,14 +6,7 @@ import ResourcesService from '../services/resources-service';
 class MyWordlist extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      modal: false
-    };
-  }
-
-  toggle = () => {
-    const currentModalValue = this.state.modal;
-    this.setState({ modal: !currentModalValue });
+    this.state = {};
   }
 
   async componentDidMount() {
@@ -41,21 +33,8 @@ class MyWordlist extends Component {
 
     return (
       <div>
-        <div>
-          <Button color="danger" onClick={this.toggle}>Modal Button</Button>
-          <Modal isOpen={this.state.modal} toggle={this.toggle}>
-            <ModalHeader toggle={this.toggle}>Modal title</ModalHeader>
-            <ModalBody>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-            </ModalBody>
-            <ModalFooter>
-              <Button color="primary" onClick={this.toggle}>Do Something</Button>{' '}
-              <Button color="secondary" onClick={this.toggle}>Cancel</Button>
-            </ModalFooter>
-          </Modal>
-        </div>
         <h3>this.state.wordlist:</h3>
-        {/* <p>{this.state.wordlist}</p> */}
+        <p>{this.state.wordlist}</p>
       </div>
     );
   }
