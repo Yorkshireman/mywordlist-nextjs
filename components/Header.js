31f8a24e5e7516f091493dc493af6f212832717b
Andrew Stelmach <Andrew.Stelmach@sky.uk>
2019-12-03 22:59:32 +0000

    strip down the header

--- components/Header.js
+++ components/Header.js
@@ -1,13 +1,39 @@
-const Header = () => (
-  <div className='Header'>
-    HEADER
-    <style jsx>{`
-      background-color: blue;
-      color: white;
-      width: 100%;
-      height: 50px
-    `}</style>
-  </div>
-);
+import React, { useState } from 'react';
+
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink
+} from 'reactstrap';
+
+const Header = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => setIsOpen(!isOpen);
+
+  return (
+    <div>
+      <Navbar color="light" light expand="md">
+        <NavbarBrand href="/">MyWordlist</NavbarBrand>
+        <NavbarToggler onClick={toggle} />
+        <Collapse isOpen={isOpen} navbar>
+          <Nav className="mr-auto" navbar>
+            <NavItem>
+              <NavLink href="/signin">Sign In</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="/mywordlist">My Wordlist</NavLink>
+            </NavItem>
+          </Nav>
+          {/* <NavbarText>Simple Text</NavbarText> */}
+        </Collapse>
+      </Navbar>
+    </div>
+  );
+}
 
 export default Header;
