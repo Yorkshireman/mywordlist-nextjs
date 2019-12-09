8069192988abf4210d40ac56dad04426587c508a
Andrew Stelmach <Andrew.Stelmach@sky.uk>
2019-12-09 01:42:29 +0000

    add modal

--- components/MyWordlist.js
+++ components/MyWordlist.js
@@ -1,4 +1,11 @@
-import { Button } from 'reactstrap';
+import {
+  Button,
+  Modal,
+  ModalBody,
+  ModalHeader,
+  ModalFooter
+} from 'reactstrap';
+
 import Error from 'next/error';
 import React, { Component } from 'react';
 
@@ -9,7 +16,14 @@ import { Wordlist } from '../models/wordlist';
 class MyWordlist extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      modal: false
+    };
+  }
+
+  togglemodal = () => {
+    const currentModalValue = this.state.modal;
+    this.setState({ modal: !currentModalValue });
   }
 
   async componentDidMount() {
@@ -37,7 +51,24 @@ class MyWordlist extends Component {
     }
 
     if (!this.state.wordlist) return null;
-    if (!this.state.wordlist.entries) return <Button>Add Word</Button>;
+    if (!this.state.wordlist.entries) {
+      return (
+        <div>
+          <Button onClick={this.togglemodal}>Add Word</Button>
+          <Modal isOpen={this.state.modal} toggle={this.togglemodal}>
+            <ModalHeader toggle={this.togglemodal}>Modal title</ModalHeader>
+            <ModalBody>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </ModalBody>
+            <ModalFooter>
+              <Button color="primary" onClick={this.togglemodal}>Do Something</Button>{' '}
+              <Button color="secondary" onClick={this.togglemodal}>Cancel</Button>
+            </ModalFooter>
+          </Modal>
+        </div>
+      );
+    }
+
     return (
       <div>
       </div>
