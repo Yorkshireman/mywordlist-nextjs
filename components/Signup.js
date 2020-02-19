7a6561353e2295e28c36a3d043db206f2384f5e0
Andrew Stelmach <Yorkshireman@users.noreply.github.com>
2020-02-19 21:52:03 +0000

    Convert to Hooks (#19)

--- components/Signup.js
+++ components/Signup.js
@@ -1,103 +1,117 @@
 import Error from 'next/error';
+import Head from 'next/head';
 import Link from 'next/link';
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import Router from 'next/router';
 
 import AuthenticationService from '../services/authentication-service';
 import ResourcesService from '../services/resources-service';
 
-class SignUp extends Component {
-  state = {
-    email: '',
-    password: '',
-    username: ''
-  }
+const SignUp = () => {
+  const [email, setEmail] = useState('');
+  const [error, setError] = useState();
+  const [loading, setLoading] = useState(false);
+  const [password, setPassword] = useState('');
+  const [username, setUsername] = useState('');
 
-  handleChange = ({ target: { name, value } }) => {
-    this.setState({ [name]: value });
-  }
+  const handleChange = ({ target: { name, value } }) => {
+    switch (name) {
+    case 'email':
+      setEmail(value);
+      break;
+    case 'password':
+      setPassword(value);
+      break;
+    case 'username':
+      setUsername(value);
+    }
+  };
 
-  setAuthToken = async response => {
+  const setAuthToken = async response => {
     const { data: { token } } = await response.json();
     window.localStorage.setItem('myWordlistAuthToken', token);
     return token;
-  }
+  };
 
-  handleSubmit = async event => {
+  const handleSubmit = async event => {
     event.preventDefault();
-    const { email, password, username } = this.state;
     let token;
     try {
+      setLoading(true);
       let response = await AuthenticationService.signUp({ email, password, username });
-      token = await this.setAuthToken(response);
+      token = await setAuthToken(response);
       response = await ResourcesService.createWordlist(token);
-      await this.setAuthToken(response);
-    } catch (error) {
+      await setAuthToken(response);
+    } catch (e) {
       window.localStorage.setItem('myWordlistAuthToken', token);
-      return this.setState({ error });
+      return setError(e);
     }
 
     Router.push('/');
-  }
+  };
 
-  render() {
-    if (this.state.error) {
-      const { name, statusCode } = this.state.error;
-      return <Error statusCode={statusCode} title={name} />;
-    }
-
-    return (
-      <div>
-        <main className='signup'>
-          <form onSubmit={this.handleSubmit}>
-            <label htmlFor='username'>Username (public)</label>
-            <input
-              type='text'
-              id='username'
-              name='username'
-              value={this.state.username}
-              onChange={this.handleChange}
-            />
-            <label htmlFor='email'>Email</label>
-            <input
-              type='text'
-              id='email'
-              name='email'
-              value={this.state.email}
-              onChange={this.handleChange}
-            />
-            <label htmlFor='password'>Password</label>
-            <input
-              type='password'
-              id='password'
-              name='password'
-              value={this.state.password}
-              onChange={this.handleChange}
-            />
-            <button type='submit'>Sign Up</button>
-          </form>
-          <Link href="/signin">
-            <a>Already have an account? Sign in</a>
-          </Link>
-        </main>
-        <style jsx>{`
-          .signup {
-            max-width: 20rem;
-            margin: 0 auto;
-            padding: 1rem;
-          }
-          form {
-            display: flex;
-            flex-flow: column;
-          }
-          input {
-            padding: 0.5rem;
-            margin: 0.3rem 0 1rem;
-          }
-        `}</style>
-      </div>
-    );
-  }
-}
+  return (
+    <div>
+      <Head>
+        <title>MyWordlist | Signup</title>
+      </Head>
+      {error ? <Error statusCode={error.statusCode} title={error.name} /> :
+        <div>
+          <main className='signup'>
+            <form onSubmit={handleSubmit}>
+              <label htmlFor='username'>Username (public)</label>
+              <input
+                type='text'
+                id='username'
+                name='username'
+                value={username}
+                onChange={handleChange}
+              />
+              <label htmlFor='email'>Email</label>
+              <input
+                type='text'
+                id='email'
+                name='email'
+                value={email}
+                onChange={handleChange}
+              />
+              <label htmlFor='password'>Password</label>
+              <input
+                type='password'
+                id='password'
+                name='password'
+                value={password}
+                onChange={handleChange}
+              />
+              {loading ? <p>Loading...</p> :
+                <button type='submit'>Sign Up</button>
+              }
+            </form>
+            <Link href="/signin">
+              <a>Already have an account? Sign in</a>
+            </Link>
+          </main>
+          <style jsx>
+            {`
+              .signup {
+                max-width: 20rem;
+                margin: 0 auto;
+                padding: 1rem;
+              }
+              form {
+                display: flex;
+                flex-flow: column;
+              }
+              input {
+                padding: 0.5rem;
+                margin: 0.3rem 0 1rem;
+              }
+            `}
+          </style>
+        </div>
+      }
+    </div>
+  );
+};
 
 export default SignUp;
