2ce3b1ff818dbb3c1b0d68750ee63a245a54bf82
Andrew Stelmach <Andrew.Stelmach@sky.uk>
2020-03-02 23:07:06 +0000

    add button which opens modal

--- components/MyWordlist.js
+++ components/MyWordlist.js
@@ -1,14 +1,13 @@
-import { FormGroup, Input, Label } from 'reactstrap';
+import { Button, FormGroup, Input, Label, Modal, ModalBody, ModalFooter, ModalHeader } from 'reactstrap';
 import { useState } from 'react';
 
 import WordlistEntry from './WordlistEntry';
 
 const MyWordlist = ({ wordlistEntriesData }) => {
   const [showDescriptions, setShowDescriptions] = useState(true);
+  const [addWordModal, setAddWordModal] = useState(false);
 
-  const handleChange = ({ target: { checked } }) => {
-    setShowDescriptions(checked);
-  };
+  const handleChange = ({ target: { checked } }) => setShowDescriptions(checked);
 
   const renderWordlistEntries = entries => entries.map((entry, index) => {
     const { description, word: { name } } = entry;
@@ -21,6 +20,8 @@ const MyWordlist = ({ wordlistEntriesData }) => {
       />
     );
   });
+
+  const toggleAddWordModal = () => setAddWordModal(!addWordModal);
 
   return (
     <>
@@ -36,6 +37,23 @@ const MyWordlist = ({ wordlistEntriesData }) => {
           </Label>
         </FormGroup>
       </div>
+      <Button
+        color='primary'
+        onClick={toggleAddWordModal}
+        style={{ position: 'fixed', bottom: '3em', right: '3em' }}
+      >
+        Add Word
+      </Button>
+      <Modal isOpen={addWordModal} toggle={toggleAddWordModal}>
+        <ModalHeader toggle={toggleAddWordModal}>Modal title</ModalHeader>
+        <ModalBody>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        </ModalBody>
+        <ModalFooter>
+          <Button color="primary" onClick={toggleAddWordModal}>Do Something</Button>{' '}
+          <Button color="secondary" onClick={toggleAddWordModal}>Cancel</Button>
+        </ModalFooter>
+      </Modal>
       {renderWordlistEntries(wordlistEntriesData)}
     </>
   );
