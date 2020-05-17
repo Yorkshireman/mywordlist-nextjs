618e31af273735ab4214399ecd8ea2e4fba94af8
Andrew Stelmach <Andrew.Stelmach@sky.uk>
2020-05-17 18:48:57 +0100

    give svg icon some transparency

--- components/AddWordIcon.js
+++ components/AddWordIcon.js
@@ -9,9 +9,9 @@ const AddWordIcon = ({ onClick }) => {
       right: '3em',
       stroke: 'grey'
     }} viewBox='0 0 100 100'>
-    <circle cx='50' cy='50' r='45' fill='none' strokeWidth='9'></circle>
-    <line x1='27.5' y1='50' x2='72.5' y2='50' strokeWidth='9'></line>
-    <line x1='50' y1='27.5' x2='50' y2='72.5' strokeWidth='9'></line>
+    <circle cx='50' cy='50' r='45' fill='none' strokeWidth='9' strokeOpacity='0.65'></circle>
+    <line x1='27.5' y1='50' x2='72.5' y2='50' strokeWidth='9' strokeOpacity='0.65'></line>
+    <line x1='50' y1='27.5' x2='50' y2='72.5' strokeWidth='9' strokeOpacity='0.65'></line>
   </svg>;
 };
 
