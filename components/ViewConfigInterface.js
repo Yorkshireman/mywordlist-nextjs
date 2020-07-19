64ff8096dfe9e11d685a6aaa509fe3f11d0afc18
Andrew Stelmach <Andrew.Stelmach@sky.uk>
2020-07-20 00:51:37 +0100

    align radio buttons to right

--- components/ViewConfigInterface.js
+++ components/ViewConfigInterface.js
@@ -1,15 +1,38 @@
 const ViewConfigInterface = ({ rSelected, rSelectedValues, setRSelected }) => {
   const { CATEGORIES, DESCRIPTIONS } = rSelectedValues;
   return (
-    <section style={{ marginBottom: '0.5em' }}>
-      <label>
-        <input type='radio' checked={rSelected === DESCRIPTIONS} onChange={() => setRSelected(DESCRIPTIONS)} />
-        Descriptions
-      </label>
-      <label>
-        <input type='radio' checked={rSelected === CATEGORIES} onChange={() => setRSelected(CATEGORIES)} />
-        Categories
-      </label>
+    <section>
+      <ul style={{ listStyle: 'none', marginBottom: '0', textAlign: 'right' }}>
+        <li>
+          <label>
+            Categories
+            <input
+              className='radio-input'
+              type='radio'
+              checked={rSelected === CATEGORIES}
+              onChange={() => setRSelected(CATEGORIES)}
+            />
+          </label>
+        </li>
+        <li>
+          <label>
+            Descriptions
+            <input
+              className='radio-input'
+              type='radio'
+              checked={rSelected === DESCRIPTIONS}
+              onChange={() => setRSelected(DESCRIPTIONS)}
+            />
+          </label>
+        </li>
+      </ul>
+      <style jsx>{`
+        .radio-input {
+          margin-left: 5px;
+          position: relative;
+          top: 1px;
+        }
+      `}</style>
     </section>
   );
 };
