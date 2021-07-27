71bdd81f888f21166c891519cf9c9f88a30e61ac
Andrew Stelmach <andrew_stelmach@hotmail.com>
2021-07-28 00:03:17 +0100

    upgrade everything

--- next.config.js
+++ next.config.js
@@ -8,5 +8,6 @@ module.exports = withCSS({
   publicRuntimeConfig: {
     AUTHENTICATION_SERVER_BASE_URL: process.env.AUTHENTICATION_SERVER_BASE_URL,
     RESOURCES_SERVER_BASE_URL: process.env.RESOURCES_SERVER_BASE_URL
-  }
+  },
+  webpack5: false
 });
